40ae5c7534125b309b2cbf3e0bc90dee1f49792a
Dan Gebhardt <dan@cerebris.com>
2020-09-19 14:14:07 -0400

    Init state with 3 todos

--- src/orbit/memorySource.ts
+++ src/orbit/memorySource.ts
@@ -4,9 +4,22 @@ import schema from './schema';
 const memorySource = new MemorySource({ schema });
 
 // Init state
-// memorySource.cache.patch(t => [
-//   t.addRecord({ type: 'todo', id: schema.generateId(), attributes: { description: 'a', completed: false }}),
-//   t.addRecord({ type: 'todo', id: schema.generateId(), attributes: { description: 'b', completed: false }})
-// ])
+memorySource.cache.patch((t) => [
+  t.addRecord({
+    type: 'todo',
+    id: schema.generateId(),
+    attributes: { description: 'a', completed: false, created: Date.now() }
+  }),
+  t.addRecord({
+    type: 'todo',
+    id: schema.generateId(),
+    attributes: { description: 'b', completed: false, created: Date.now() + 1 }
+  }),
+  t.addRecord({
+    type: 'todo',
+    id: schema.generateId(),
+    attributes: { description: 'c', completed: false, created: Date.now() + 2 }
+  })
+]);
 
 export default memorySource;
